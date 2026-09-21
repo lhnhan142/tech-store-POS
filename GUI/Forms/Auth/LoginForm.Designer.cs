1443296963f692e9d54dcb761e7f0ae8788692df
lhnhan142 <lhnhan142@gmail.com>
2026-09-21 16:05:03 +0700

    Chuyển qua dùng Siticone:UI

--- GUI/Forms/Auth/LoginForm.Designer.cs
+++ GUI/Forms/Auth/LoginForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class LoginForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,56 +15,40 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
-            guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
-            txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
-            txtUsername = new Guna.UI2.WinForms.Guna2TextBox();
-            chkRememberMe = new Guna.UI2.WinForms.Guna2ToggleSwitch();
+            SiticoneBorderlessForm1 = new Siticone.Desktop.UI.WinForms.SiticoneBorderlessForm(components);
+            SiticoneControlBox1 = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
+            txtPassword = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            txtUsername = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            chkRememberMe = new Siticone.Desktop.UI.WinForms.SiticoneToggleSwitch();
             lblRemenberMe = new Label();
-            btnLogin = new Guna.UI2.WinForms.Guna2Button();
-            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            btnLogin = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            SiticoneHtmlLabel1 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
             SuspendLayout();
             // 
-            // guna2BorderlessForm1
+            // SiticoneBorderlessForm1
             // 
-            guna2BorderlessForm1.BorderRadius = 15;
-            guna2BorderlessForm1.ContainerControl = this;
-            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
-            guna2BorderlessForm1.TransparentWhileDrag = true;
+            SiticoneBorderlessForm1.BorderRadius = 15;
+            SiticoneBorderlessForm1.ContainerControl = this;
+            SiticoneBorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            SiticoneBorderlessForm1.TransparentWhileDrag = true;
             // 
-            // guna2ControlBox1
+            // SiticoneControlBox1
             // 
-            guna2ControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            guna2ControlBox1.CustomizableEdges = customizableEdges9;
-            guna2ControlBox1.FillColor = Color.FromArgb(139, 152, 166);
-            guna2ControlBox1.IconColor = Color.White;
-            guna2ControlBox1.Location = new Point(743, -2);
-            guna2ControlBox1.Name = "guna2ControlBox1";
-            guna2ControlBox1.ShadowDecoration.CustomizableEdges = customizableEdges10;
-            guna2ControlBox1.Size = new Size(56, 36);
-            guna2ControlBox1.TabIndex = 0;
+            SiticoneControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            SiticoneControlBox1.FillColor = Color.FromArgb(139, 152, 166);
+            SiticoneControlBox1.IconColor = Color.White;
+            SiticoneControlBox1.Location = new Point(743, -2);
+            SiticoneControlBox1.Name = "SiticoneControlBox1";
+            SiticoneControlBox1.Size = new Size(56, 36);
+            SiticoneControlBox1.TabIndex = 0;
             // 
             // txtPassword
             // 
+            txtPassword.BackColor = Color.Transparent;
             txtPassword.BorderRadius = 8;
-            txtPassword.CustomizableEdges = customizableEdges7;
             txtPassword.DefaultText = "";
             txtPassword.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
             txtPassword.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
@@ -83,17 +60,17 @@
             txtPassword.Location = new Point(261, 228);
             txtPassword.Margin = new Padding(3, 4, 3, 4);
             txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '●';
             txtPassword.PlaceholderText = "Mật khẩu";
             txtPassword.SelectedText = "";
-            txtPassword.ShadowDecoration.CustomizableEdges = customizableEdges8;
             txtPassword.Size = new Size(286, 60);
             txtPassword.TabIndex = 1;
             txtPassword.UseSystemPasswordChar = true;
             // 
             // txtUsername
             // 
+            txtUsername.BackColor = Color.Transparent;
             txtUsername.BorderRadius = 8;
-            txtUsername.CustomizableEdges = customizableEdges5;
             txtUsername.DefaultText = "";
             txtUsername.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
             txtUsername.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
@@ -105,22 +82,21 @@
             txtUsername.Location = new Point(261, 137);
             txtUsername.Margin = new Padding(3, 4, 3, 4);
             txtUsername.Name = "txtUsername";
+            txtUsername.PasswordChar = '\0';
             txtUsername.PlaceholderText = "Tên Đăng Nhập";
             txtUsername.SelectedText = "";
-            txtUsername.ShadowDecoration.CustomizableEdges = customizableEdges6;
             txtUsername.Size = new Size(286, 60);
             txtUsername.TabIndex = 2;
             // 
             // chkRememberMe
             // 
+            chkRememberMe.BackColor = Color.Transparent;
             chkRememberMe.CheckedState.BorderColor = Color.FromArgb(94, 148, 255);
             chkRememberMe.CheckedState.FillColor = Color.FromArgb(94, 148, 255);
             chkRememberMe.CheckedState.InnerBorderColor = Color.White;
             chkRememberMe.CheckedState.InnerColor = Color.White;
-            chkRememberMe.CustomizableEdges = customizableEdges3;
-            chkRememberMe.Location = new Point(405, 307);
+            chkRememberMe.Location = new Point(451, 307);
             chkRememberMe.Name = "chkRememberMe";
-            chkRememberMe.ShadowDecoration.CustomizableEdges = customizableEdges4;
             chkRememberMe.Size = new Size(44, 25);
             chkRememberMe.TabIndex = 3;
             chkRememberMe.UncheckedState.BorderColor = Color.FromArgb(125, 137, 149);
@@ -131,40 +107,42 @@
             // lblRemenberMe
             // 
             lblRemenberMe.AutoSize = true;
-            lblRemenberMe.Location = new Point(261, 307);
+            lblRemenberMe.BackColor = Color.Transparent;
+            lblRemenberMe.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRemenberMe.Location = new Point(261, 304);
             lblRemenberMe.Name = "lblRemenberMe";
-            lblRemenberMe.Size = new Size(138, 20);
+            lblRemenberMe.Size = new Size(184, 28);
             lblRemenberMe.TabIndex = 4;
             lblRemenberMe.Text = "Ghi nhớ đăng nhập:";
             // 
             // btnLogin
             // 
+            btnLogin.BackColor = Color.Transparent;
             btnLogin.BorderRadius = 8;
-            btnLogin.CustomizableEdges = customizableEdges1;
             btnLogin.DisabledState.BorderColor = Color.DarkGray;
             btnLogin.DisabledState.CustomBorderColor = Color.DarkGray;
             btnLogin.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             btnLogin.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
             btnLogin.Font = new Font("Segoe UI", 9F);
             btnLogin.ForeColor = Color.White;
+            btnLogin.HoverState.FillColor = SystemColors.ActiveCaption;
             btnLogin.Location = new Point(296, 354);
             btnLogin.Name = "btnLogin";
-            btnLogin.ShadowDecoration.CustomizableEdges = customizableEdges2;
             btnLogin.Size = new Size(225, 56);
             btnLogin.TabIndex = 5;
             btnLogin.Text = "ĐĂNG NHẬP";
             btnLogin.Click += btnLogin_Click;
             // 
-            // guna2HtmlLabel1
+            // SiticoneHtmlLabel1
             // 
-            guna2HtmlLabel1.BackColor = Color.Transparent;
-            guna2HtmlLabel1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            guna2HtmlLabel1.Location = new Point(207, 52);
-            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            guna2HtmlLabel1.Size = new Size(381, 47);
-            guna2HtmlLabel1.TabIndex = 6;
-            guna2HtmlLabel1.Text = "ĐĂNG NHẬP HỆ THỐNG";
-            guna2HtmlLabel1.TextAlignment = ContentAlignment.MiddleCenter;
+            SiticoneHtmlLabel1.BackColor = Color.Transparent;
+            SiticoneHtmlLabel1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SiticoneHtmlLabel1.Location = new Point(207, 52);
+            SiticoneHtmlLabel1.Name = "SiticoneHtmlLabel1";
+            SiticoneHtmlLabel1.Size = new Size(381, 47);
+            SiticoneHtmlLabel1.TabIndex = 6;
+            SiticoneHtmlLabel1.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            SiticoneHtmlLabel1.TextAlignment = ContentAlignment.MiddleCenter;
             // 
             // LoginForm
             // 
@@ -173,13 +151,13 @@
             BackgroundImage = Properties.Resources.dried_leaf_frame_botanical_background;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
-            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(SiticoneHtmlLabel1);
             Controls.Add(btnLogin);
             Controls.Add(lblRemenberMe);
             Controls.Add(chkRememberMe);
             Controls.Add(txtUsername);
             Controls.Add(txtPassword);
-            Controls.Add(guna2ControlBox1);
+            Controls.Add(SiticoneControlBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "LoginForm";
             Text = "LoginForm";
@@ -189,13 +167,13 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
-        private Guna.UI2.WinForms.Guna2TextBox txtUsername;
-        private Guna.UI2.WinForms.Guna2TextBox txtPassword;
-        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
-        private Guna.UI2.WinForms.Guna2Button btnLogin;
-        private Label lblRemenberMe;
-        private Guna.UI2.WinForms.Guna2ToggleSwitch chkRememberMe;
-        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Siticone.Desktop.UI.WinForms.SiticoneBorderlessForm SiticoneBorderlessForm1;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtUsername;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtPassword;
+        private Siticone.Desktop.UI.WinForms.SiticoneControlBox SiticoneControlBox1;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton btnLogin;
+        private System.Windows.Forms.Label lblRemenberMe;
+        private Siticone.Desktop.UI.WinForms.SiticoneToggleSwitch chkRememberMe;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel SiticoneHtmlLabel1;
     }
 }
